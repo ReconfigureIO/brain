1f926607a0daf6f88f7a68f23a0f0c2db70ab50b
Mahdi Jelodari <mamagham@cs.man.ac.uk>
2017-10-19 16:36:10 +0100

    fix type mismatch

--- examples/inference/cmd/test-2x3x2/main.go
+++ examples/inference/cmd/test-2x3x2/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"xcl"
-	"github.com/reconfigureio/brain/bnn"
-	"github.com/reconfigureio/brain/utils"
+	"os"
+//	"github.com/reconfigureio/brain/bnn"
+//	"github.com/reconfigureio/brain/utils"
+	"github.com/reconfigureio/fixed"
 )
 
 //Partition example dataset based on BATCH_SIZE
@@ -20,17 +23,50 @@ func main() {
 	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
 	defer krnl.Release()
 
-	var fpath string
-	input = utils.load_data(fpath)
+//	var fpath string
+//	input = utils.load_data(fpath)
 
 	//load validations 
-	test := bnn.ReadImage("dataset")
-	fmt.Println(test)
+//	test := bnn.ReadImage("dataset")
+//	fmt.Println(test)
 
 	//reshape image 
-	nw_image:= bnn.ReshapeImage(image)
-	fmt.Println(nw_image)
+//	nw_image:= bnn.ReshapeImage(image)
+//	fmt.Println(nw_image)
 
+
+	// Allocate a buffer on the FPGA to store the return value of our computation
+	// The output is a uint32, so we need 4 bytes to store it
+	buff := world.Malloc(xcl.WriteOnly, 4)
+	defer buff.Free()
+
+	// Pass the arguments to the kernel
+	a := fixed.I26(0)
+	b := fixed.I26(1)
+
+	// Set the first operand
+	krnl.SetArg(0, uint32(a))
+	// Set the second operand
+	krnl.SetArg(1, uint32(b))
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(2, buff)
+
+	// Run the kernel with the supplied arguments
 	krnl.Run(1, 1, 1)
-	fmt.Println("job's done!")
+
+	// Decode that byte slice into the uint32 we're expecting
+	var ret fixed.Int26_6
+	err := binary.Read(buff.Reader(), binary.LittleEndian, &ret)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+
+	expected := a ^ b 
+
+	// Exit with an error if the value is not correct
+	if expected != ret {
+		// Print the value we got from the FPGA
+		fmt.Printf("Expected %d, got %d\n", expected, ret)
+		os.Exit(1)
+	}
 }
--- examples/inference/main.go
+++ examples/inference/main.go
@@ -37,8 +37,8 @@ const HID_LAYER_SIZE int = 16
 const OUT_LAYER_SIZE int = 1
 
 func TOP(
-	a int64,
-	b int64,
+	a int32,
+	b int32,
 	addr uintptr,
 	// The first set of arguments will be the ports for interacting with host 
 	//output fixed.Int26_6,
@@ -51,33 +51,33 @@ func TOP(
 	memWriteResp <-chan axiprotocol.WriteResp){
 
 	//cast rawdate to input vars
-	training_data := [][]fixed.Int52_12{
-    		 []fixed.Int52_12{0, 0},
-    		 []fixed.Int52_12{0, 1},
-		 []fixed.Int52_12{1, 0},
-		 []fixed.Int52_12{1, 1}}
+/*	training_data := [][]fixed.Int26_6{
+    		 []fixed.Int26_6{0, 0},
+    		 []fixed.Int26_6{0, 1},
+		 []fixed.Int26_6{1, 0},
+		 []fixed.Int26_6{1, 1}}
 
-	target_data := []fixed.Int52_12{
+	target_data := []fixed.Int26_6{
     		 0,
     		 1,
 		 1,
 		 0}
+*/
+/*	test_data := [4]fixed.Int26_6{
+    		 fixed.Int26_6{0, 1},
+    		 fixed.Int26_6{1, 1},
+		 fixed.Int26_6{1, 0},
+		 fixed.Int26_6{0, 0}}
 
-/*	test_data := [4]fixed.Int52_12{
-    		 fixed.Int52_12{0, 1},
-    		 fixed.Int52_12{1, 1},
-		 fixed.Int52_12{1, 0},
-		 fixed.Int52_12{0, 0}}
-
-	acc_data := []fixed.Int52_12{
+	acc_data := []fixed.Int26_6{
     		 1,
     		 0,
 		 1,
 		 0}
 */
 	//weights exported from xornet on KERAS (epoch size = 500 - sgd)
-/*	weights := [][]fixed.Int52_12{
- 		[]fixed.Int52_12{-0.35589939,
+/*	weights := [][]fixed.Int26_6{
+ 		[]fixed.Int26_6{-0.35589939,
        		  0.13612342,
        		 -0.27676189,
        		 -0.06193029,
@@ -92,44 +92,44 @@ func TOP(
        		  0.54061526,
        		 -0.42877936,
        		  0.54952145,
-       		  0.19469711},[]fixed.Int52_12{-0.08784658}}*/
+       		  0.19469711},[]fixed.Int26_6{-0.08784658}}*/
 
 	//weights exported from xornet on KERAS (epoch size = 5000 - adam)
-	weights_h := [16]fixed.Int52_12{
-		 fixed.I52F(0, 1726144),
-		 fixed.I52F(-2, 10709),
-       		 fixed.I52F(0, 43040475),
-	         fixed.I52F(-0, 36798), //?
-       		 fixed.I52F(-2 ,14761877),
-       		 fixed.I52F(1 ,65221334),
-       		 fixed.I52F(-0, 47918937),
-       		 fixed.I52F(-2 ,28618431),
-      		 fixed.I52F(-1, 64216483),
-       		 fixed.I52F(1, 45400071),
-       		 fixed.I52F(0, 8930543), //?
-       		 fixed.I52F(-1,85224831),
-       		 fixed.I52F(1,3171016),
-     		 fixed.I52F(-1,74173605),
-       		 fixed.I52F(-0,37978798),
-       		 fixed.I52F(-2,9490085)}
+	weights_h := [16]fixed.Int26_6{
+		 fixed.I26F(0, 1726144),
+		 fixed.I26F(-2, 10709),
+       		 fixed.I26F(0, 43040475),
+	         fixed.I26F(-0, 36798), //?
+       		 fixed.I26F(-2 ,14761877),
+       		 fixed.I26F(1 ,65221334),
+       		 fixed.I26F(-0, 47918937),
+       		 fixed.I26F(-2 ,28618431),
+      		 fixed.I26F(-1, 64216483),
+       		 fixed.I26F(1, 45400071),
+       		 fixed.I26F(0, 8930543), //?
+       		 fixed.I26F(-1,85224831),
+       		 fixed.I26F(1,3171016),
+     		 fixed.I26F(-1,74173605),
+       		 fixed.I26F(-0,37978798),
+       		 fixed.I26F(-2,9490085)}
 
-	weights_o := fixed.Int52_12{fixed.I52F(0, 46938747)} //09490085??
+	weights_o := fixed.I26F(0, 46938747) //09490085??
 
 	//build a network with 3 layers of input, hidden, and output
 //	layer_in := NetworkLayer(INP_LAYER_SIZE,"relu")
 //	layer_hidden := NetworkLayer(HID_LAYER_SIZE,"relu")
 //	layer_out := NetworkLayer(OUT_LAYER_SIZE,"sig")
 
-	var layer_in [INP_LAYER_SIZE]fixed.Int52_12  //"relu"
-	var layer_hidden [HID_LAYER_SIZE]fixed.Int52_12 //"relu")
-	var layer_out [OUT_LAYER_SIZE]fixed.Int52_12 //"sig"
+	var layer_in [INP_LAYER_SIZE]fixed.Int26_6  //"relu"
+	var layer_hidden [HID_LAYER_SIZE]fixed.Int26_6 //"relu")
+	var layer_out [OUT_LAYER_SIZE]fixed.Int26_6 //"sig"
 
 	// Since we're not reading anything from memory, disable those reads
 	go axiprotocol.ReadDisable(memReadAddr, memReadData)
 
 	//Initialize the first layer
-	layer_in[0] = fixed.Int52_12(a)
-	layer_in[1] = fixed.Int52_12(b)
+	layer_in[0] = fixed.Int26_6(a)
+	layer_in[1] = fixed.Int26_6(b)
 
 /*	for i := 0; i < INP_LAYER_SIZE ; i++{
 	 
@@ -146,7 +146,7 @@ func TOP(
 		layer_hidden[i] = out  
  	}
 	//Calculate outval for the output layer
-	sum := fixed.Int52_12(0)
+	sum := fixed.Int26_6(0)
 	for i := 0; i < OUT_LAYER_SIZE ; i++{
 	 
 		sum += layer_hidden[i]
