a71d379fc1f7b55178a489f0f9cb69bd01689f8b
Mahdi Jelodari <mamagham@cs.man.ac.uk>
2017-10-30 15:18:17 +0000

    add benchmark

--- examples/inference/cmd/test-2x3x2/main.go
+++ examples/inference/cmd/test-2x3x2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"xcl"
 	"os"
+        "testing"
 //	"github.com/reconfigureio/brain/bnn"
 //	"github.com/reconfigureio/brain/utils"
 	"github.com/reconfigureio/fixed"
@@ -15,6 +16,22 @@ import (
 //the output accuracy achieved from the model    
 const NUM_EPOCHS int = 100
 const BATCH_SIZE int = 500
+
+func BenchmarkKernel(world xcl.World, krnl *xcl.Kernel, B *testing.B, a fixed.Int26_6, b fixed.Int26_6, buff *xcl.Memory) {
+
+
+
+	// Set the first operand
+	krnl.SetArg(0, uint32(a))
+	// Set the second operand
+	krnl.SetArg(1, uint32(b))
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(2, buff)
+
+	// Reset the timer so that we only measure runtime of the kernel
+	B.ResetTimer()
+	krnl.Run(1, 1, 1)
+}
 
 func main() {
 	world := xcl.NewWorld()
@@ -34,25 +51,24 @@ func main() {
 //	nw_image:= bnn.ReshapeImage(image)
 //	fmt.Println(nw_image)
 
-
-	// Allocate a buffer on the FPGA to store the return value of our computation
-	// The output is a uint32, so we need 4 bytes to store it
-	buff := world.Malloc(xcl.WriteOnly, 4)
-	defer buff.Free()
+        // Allocate a buffer on the FPGA to store the return value of our computation
+        // The output is a uint32, so we need 4 bytes to store it
+        buff := world.Malloc(xcl.WriteOnly, 4)
+        defer buff.Free()
 
 	// Pass the arguments to the kernel
 	a := fixed.I26(0)
 	b := fixed.I26(1)
 
-	// Set the first operand
-	krnl.SetArg(0, uint32(a))
-	// Set the second operand
-	krnl.SetArg(1, uint32(b))
-	// Set the pointer to the output buffer
-	krnl.SetMemoryArg(2, buff)
+	// Create a function that the benchmarking machinery can call
+	f := func(B *testing.B) {
+		BenchmarkKernel(world, krnl, B, a, b, buff)
+	}
+	// Benchmark it
+	result := testing.Benchmark(f)
 
-	// Run the kernel with the supplied arguments
-	krnl.Run(1, 1, 1)
+	// Print the result
+	fmt.Printf("%s\n", result.String())
 
 	// Decode that byte slice into the uint32 we're expecting
 	var ret fixed.Int26_6
@@ -61,6 +77,7 @@ func main() {
 		fmt.Println("binary.Read failed:", err)
 	}
 
+	// Compute the expected result 
 	expected := a ^ b 
 
 	// Exit with an error if the value is not correct
@@ -69,4 +86,5 @@ func main() {
 		fmt.Printf("Expected %d, got %d\n", expected, ret)
 		os.Exit(1)
 	}
+
 }
