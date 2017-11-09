edc539606b0ad6df438d2675d93c26848ac899be
Mahdi Jelodari <mamagham@cs.man.ac.uk>
2017-11-09 12:13:00 +0000

    set up activation, weight and bias tables

--- examples/inference/cmd/test-2x3x2/main.go
+++ examples/inference/cmd/test-2x3x2/main.go
@@ -17,22 +17,32 @@ import (
 const NUM_EPOCHS int = 100
 const BATCH_SIZE int = 500
 
-func BenchmarkKernel(world xcl.World, krnl *xcl.Kernel, B *testing.B, buffIn *xcl.Memory, buffActs *xcl.Memory, buffOut *xcl.Memory) {
+func BenchmarkKernel(world xcl.World, 
+		krnl *xcl.Kernel,
+		B *testing.B, 
+		buffActs *xcl.Memory, 
+		buffIn *xcl.Memory, 
+		buffWeightH *xcl.Memory,
+		buffBiasH *xcl.Memory,
+		buffWeightO *xcl.Memory,
+		buffBiasO *xcl.Memory,
+		buffOut *xcl.Memory) {
 
-
-
-/*	// Set the first operand
-	krnl.SetArg(0, uint32(a))
-	// Set the second operand
-	krnl.SetArg(1, uint32(b))
-*/
 
 	// Set the pointer to the output buffer
-	krnl.SetMemoryArg(0, buffIn)
+	krnl.SetMemoryArg(0, buffActs)
 	// Set the pointer to the output buffer
-	krnl.SetMemoryArg(1, buffActs)
+	krnl.SetMemoryArg(1, buffIn)
 	// Set the pointer to the output buffer
-	krnl.SetMemoryArg(2, buffOut)
+	krnl.SetMemoryArg(2, buffWeightH)
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(3, buffBiasH)
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(4, buffWeightO)
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(5, buffBiasO)
+	// Set the pointer to the output buffer
+	krnl.SetMemoryArg(6, buffOut)
 
 
 	// Reset the timer so that we only measure runtime of the kernel
@@ -59,7 +69,7 @@ func main() {
 //	fmt.Println(nw_image)
 
 
-  //generated table by util/disretise_sig(1)
+	// Generated table by util/disretise_sig(1)
   	actives := [200]fixed.Int26_6{fixed.I26F(0 , 0 << 0), 
 		fixed.I26F(0 , 0 << 0), 
 		fixed.I26F(0 , 0 << 0), 
@@ -261,19 +271,73 @@ func main() {
 		fixed.I26F(1 , 0 << 0), 
 		fixed.I26F(1 , 0 << 0)}
 
+	// Input batch size = 4 - famous iris flower dataset
+	inp := [4]fixed.Int26_6{fixed.I26F(0 , 583333333333 << 0),
+		fixed.I26F(0 , 291666666667 << 0),
+		fixed.I26F(0 , 728813559322 << 0),
+		fixed.I26F(0 , 75 << 0)}
 
-	inp := []fixed.Int26_6{0, 1}
-//	inpSize := binary.Size(inp)
+	// From the training stage of datadan.io network model
+	weightH := [12]fixed.Int26_6{fixed.I26F(-9 , 664649023077196 << 0),
+		fixed.I26F(-3 , 331748963332474 << 0),
+		fixed.I26F(0 , 4798734558954657 >> 1),
+		fixed.I26F(-9 , 16865031545236 << 0),
+		fixed.I26F(7 , 526678142973989 << 0),
+		fixed.I26F(-11 , 259862681673592 << 0),
+		fixed.I26F(35 , 375442386538865 << 0),
+		fixed.I26F(-8 , 377651024226779 << 0),
+		fixed.I26F(0 , 5857331473302626 << 0),
+		fixed.I26F(14 , 569948798276545 << 0),
+		fixed.I26F(-6 , 977871499870339 << 0),
+		fixed.I26F(-3 , 595834572863486 << 0)}
+
+	biasH := [3]fixed.Int26_6{fixed.I26F(-24 , 257317924965545 << 0),
+		fixed.I26F(2 , 841569482220003 << 0),
+		fixed.I26F(1 , 2344415303891234 << 0)}
+
+	weightO := [9]fixed.Int26_6{fixed.I26F(-6 , 421059674610277 << 0),
+		fixed.I26F(10 , 430255115994242 << 0),
+		fixed.I26F(-10 , 466201644889994 << 0),
+		fixed.I26F(12 , 38461165673367 << 0),
+		fixed.I26F(-5 , 610451231603859 << 0),
+		fixed.I26F(-11 , 310357612664564 << 0),
+		fixed.I26F(-12 , 155845492268348 << 0),
+		fixed.I26F(-4 , 313406702633787 << 0),
+		fixed.I26F(1 , 3793360362443159 << 0)}
+
+	biasO := [3]fixed.Int26_6{fixed.I26F(-4 , 703941034009741 << 0),
+		fixed.I26F(-6 , 59314294559707 >> 1),
+		fixed.I26F(6 , 201619726339744 << 0)}
+
 
         // Allocate a buffer on the FPGA to store the return value of our computation
-        // The output is a uint32, so we need 4 bytes to store it
-        buffIn := world.Malloc(xcl.ReadOnly, 8)
+        // The activations is a 200-uint32 set, so we need 4 * 200 bytes to store it
+        buffActs := world.Malloc(xcl.ReadOnly, 800)
+        defer buffActs.Free()
+
+        // Allocate a buffer on the FPGA to store the return value of our computation
+        // The input is a 4-uint32 set, so we need 4 * 4 bytes to store it
+        buffIn := world.Malloc(xcl.ReadOnly, 16)
         defer buffIn.Free()
 
+        // Allocate a buffer on the FPGA to store the return value of our computation
+        // The hidden-weights is a 12-uint32 set, so we need 4 * 12 bytes to store it
+        buffWeightH := world.Malloc(xcl.ReadOnly, 48)
+        defer buffActs.Free()
 
         // Allocate a buffer on the FPGA to store the return value of our computation
-        // The output is a uint32, so we need 4 bytes to store it
-        buffActs := world.Malloc(xcl.ReadOnly, 800)
+        // The hidden biases is a 3-uint32 set, so we need 4 * 3 bytes to store it
+        buffBiasH := world.Malloc(xcl.ReadOnly, 12)
+        defer buffActs.Free()
+
+        // Allocate a buffer on the FPGA to store the return value of our computation
+        // The out weights is a 9-uint32 set, so we need 4 * 9 bytes to store it
+        buffWeightO := world.Malloc(xcl.ReadOnly, 36)
+        defer buffActs.Free()
+
+        // Allocate a buffer on the FPGA to store the return value of our computation
+        // The out biases is a 3-uint32 set, so we need 4 * 3 bytes to store it
+        buffBiasO := world.Malloc(xcl.ReadOnly, 12)
         defer buffActs.Free()
 
         // Allocate a buffer on the FPGA to store the return value of our computation
@@ -281,21 +345,25 @@ func main() {
         buffOut := world.Malloc(xcl.WriteOnly, 4)
         defer buffOut.Free()
 
-/*
-	// Set the arguments to the kernel
-	a := fixed.I26(0)
-	b := fixed.I26(1)
-	
-*/
-	binary.Write(buffIn.Writer(), binary.LittleEndian, inp)
-	//numBlocks := uint32(inpSize / 64)
+
+	// Write into the allocated buffers of inps, acts, weights and bias
 
 	binary.Write(buffActs.Writer(), binary.LittleEndian, actives)
-	//numBlocks := uint32(inpSize / 64)
+
+	binary.Write(buffIn.Writer(), binary.LittleEndian, inp)
+
+	binary.Write(buffWeightH.Writer(), binary.LittleEndian, weightH)
+
+	binary.Write(buffBiasH.Writer(), binary.LittleEndian, biasH)
+
+	binary.Write(buffWeightO.Writer(), binary.LittleEndian, weightO)
+
+	binary.Write(buffBiasO.Writer(), binary.LittleEndian, biasO)
+
 
 	// Create a function that the benchmarking machinery can call
 	f := func(B *testing.B) {
-		BenchmarkKernel(world, krnl, B, buffIn, buffActs, buffOut)
+		BenchmarkKernel(world, krnl, B, buffActs, buffIn, buffWeightH, buffBiasH, buffWeightO, buffBiasO, buffOut)
 	}
 	// Benchmark it
 	result := testing.Benchmark(f)
@@ -304,19 +372,19 @@ func main() {
 	fmt.Printf("%s\n", result.String())
 
 	// Decode that byte slice into the uint32 we're expecting
-	var ret fixed.Int26_6
+	var ret []fixed.Int26_6
 	err := binary.Read(buffOut.Reader(), binary.LittleEndian, &ret)
 	if err != nil {
 		fmt.Println("binary.Read failed:", err)
 	}
 
 	// Compute the expected result 
-	expected := inp[0] ^ inp[1] 
+	expected := [3]fixed.Int26_6{0,1,0}
 
 	// Exit with an error if the value is not correct
-	if expected != ret {
+	if expected[1] != ret[1] {
 		// Print the value we got from the FPGA
-		fmt.Printf("Expected %d, got %d\n", expected, ret)
+		fmt.Printf("Expected %d, got %d\n", expected[1], ret[1])
 		os.Exit(1)
 	}
 
--- examples/inference/main.go
+++ examples/inference/main.go
@@ -30,252 +30,19 @@ import (
 //	"github.com/reconfigureio/add"
 )
 
-
-
-
 const INP_LAYER_SIZE int = 4
 const HID_LAYER_SIZE int = 3
 const OUT_LAYER_SIZE int = 3
 
-/*
-// TODO Use Xilinx's full precision LogiCore IP
-// for math.Exp. For now a lookup table is imported instead.
-func sigmoid_act(x fixed.Int26_6) fixed.Int26_6{
-
-  //generated table by util/disretise_sig(1)
-  prime := [16]fixed.Int26_6{
-
-/*fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-		fixed.I26F(0 , 0 << 0), 
-
-		fixed.I26F(0 , 2 << 0), 
-		fixed.I26F(0 , 6 << 0), 
-		fixed.I26F(0 , 16 << 0), 
-		fixed.I26F(0 , 45 << 0), 
-		fixed.I26F(0 , 123 << 0), 
-		fixed.I26F(0 , 335 << 0), 
-		fixed.I26F(0 , 911 << 0), 
-		fixed.I26F(0 , 2472 << 0), 
-		fixed.I26F(0 , 6692 << 0), 
-		fixed.I26F(0 , 17986 << 0), 
-		fixed.I26F(0 , 47425 << 0), 
-		fixed.I26F(0 , 119202 << 0), 
-		fixed.I26F(0 , 268941 << 0), 
-		fixed.I26F(0 , 500000 << 0), 
-		fixed.I26F(0 , 731058 << 0), 
-		fixed.I26F(0 , 880797 << 0)}
-/* 
-		fixed.I26F(0 , 952574 << 0), 
-		fixed.I26F(0 , 982013 << 0), 
-		fixed.I26F(0 , 993307 << 0), 
-		fixed.I26F(0 , 997527 << 0), 
-		fixed.I26F(0 , 999088 << 0)*/
-
-/*, 
-		fixed.I26F(0 , 999664 << 0), 
-		fixed.I26F(0 , 999876 << 0), 
-		fixed.I26F(0 , 999954 << 0), 
-		fixed.I26F(0 , 999983 << 0), 
-		fixed.I26F(0 , 999993 << 0), 
-		fixed.I26F(0 , 999997 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0), 
-		fixed.I26F(0 , 999999 << 0)
-} 
-
-
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0), 
-		fixed.I26F(1 , 0 << 0)
-
-        // Convert x (26_6) to index
-	// add by (200*z)/2 index  
-	return prime[uint8(x) + 100]
-
-	if (uint8(x) + 100) < 87 {
-
-		return 0
-
-	} else if (uint8(x) + 100) < 137 {
-
-		return prime[uint8(x) + 100 - 87]
-
-	} else {
-
-		return 1	
-	}
-
-}*/
-
 func Top(
-	addrIn uintptr,
 	addrAct uintptr,
+	addrIn uintptr,
+	addrWH uintptr,
+	addrBH uintptr,
+	addrWO uintptr,
+	addrBO uintptr,
 	addrOut uintptr,
+
 	// The first set of arguments will be the ports for interacting with host 
 	//output fixed.Int26_6,
 	// The second set of arguments will be the ports for interacting with memory
@@ -286,104 +53,21 @@ func Top(
 	memWriteData chan<- axiprotocol.WriteData,
 	memWriteResp <-chan axiprotocol.WriteResp){
 
-
-	//cast rawdate to input vars
-/*	training_data := [][]fixed.Int26_6{
-    		 []fixed.Int26_6{0, 0},
-    		 []fixed.Int26_6{0, 1},
-		 []fixed.Int26_6{1, 0},
-		 []fixed.Int26_6{1, 1}}
-
-	target_data := []fixed.Int26_6{
-    		 0,
-    		 1,
-		 1,
-		 0}
-*/
-/*	test_data := [4]fixed.Int26_6{
-    		 fixed.Int26_6{0, 1},
-    		 fixed.Int26_6{1, 1},
-		 fixed.Int26_6{1, 0},
-		 fixed.Int26_6{0, 0}}
-
-	acc_data := []fixed.Int26_6{
-    		 1,
-    		 0,
-		 1,
-		 0}
-*/
-	//weights exported from xornet on KERAS (epoch size = 500 - sgd)
-/*	weights := [][]fixed.Int26_6{
- 		[]fixed.Int26_6{-0.35589939,
-       		  0.13612342,
-       		 -0.27676189,
-       		 -0.06193029,
-       		 -0.37450755,
-       		  0.48630142,
-       		  0.40621114,
-       		  0.11644399,
-       		 -0.33843306,
-       		  0.34775987,
-       		 -0.14313582,
-       		 -0.04034447,
-       		  0.54061526,
-       		 -0.42877936,
-       		  0.54952145,
-       		  0.19469711},[]fixed.Int26_6{-0.08784658}}*/
-
-	//weights exported from xornet on KERAS (epoch size = 5000 - adam)
-	weights_h := [16]fixed.Int26_6{
-		 fixed.I26F(0, 1726144),
-		 fixed.I26F(-2, 10709),
-       		 fixed.I26F(0, 43040475),
-	         fixed.I26F(-0, 36798), //?
-       		 fixed.I26F(-2 ,14761877),
-       		 fixed.I26F(1 ,65221334),
-       		 fixed.I26F(-0, 47918937),
-       		 fixed.I26F(-2 ,28618431),
-      		 fixed.I26F(-1, 64216483),
-       		 fixed.I26F(1, 45400071),
-       		 fixed.I26F(0, 8930543), //?
-       		 fixed.I26F(-1,85224831),
-       		 fixed.I26F(1,3171016),
-     		 fixed.I26F(-1,74173605),
-       		 fixed.I26F(-0,37978798),
-       		 fixed.I26F(-2,9490085)}
-
-	weights_o := fixed.I26F(0, 46938747) //09490085??
-
-//	acts := [64]fixed.Int26_6{0}
-
-//	ch := make(chan uint32, 2)
-
 	//build a network with 3 layers of input, hidden, and output
 //	layer_in := NetworkLayer(INP_LAYER_SIZE,"relu")
 //	layer_hidden := NetworkLayer(HID_LAYER_SIZE,"relu")
 //	layer_out := NetworkLayer(OUT_LAYER_SIZE,"sig")
 
-	var layer_in [INP_LAYER_SIZE]fixed.Int26_6  //"relu"
-	var layer_hidden [HID_LAYER_SIZE]fixed.Int26_6 //"relu")
-	var layer_out [OUT_LAYER_SIZE]fixed.Int26_6 //"sig"
-
+	var layer_in [INP_LAYER_SIZE]fixed.Int26_6  
+	var layer_hidden [HID_LAYER_SIZE]fixed.Int26_6 
+	var layer_out [OUT_LAYER_SIZE]fixed.Int26_6 
 
 
 	// Since we're not reading anything from memory, disable those reads
-//	go axiprotocol.ReadDisable(memReadAddr, memReadData)
-
-	// Write it back to the pointer the host requests
-	
-	//Initialize the first layer
-//	layer_in[0] = fixed.Int26_6(aximemory.ReadUInt32(memReadAddr, memReadData, false, addrIn))
-//	layer_in[1] = fixed.Int26_6(aximemory.ReadUInt32(memReadAddr, memReadData, false, addrIn+4))
-
-//        aximemory.ReadBurstUInt32(memReadAddr, memReadData, false, addrAct, 2, ch )
-
-//	for i := 0; i < 2 ; i++{
-	 
-//	 acts[i] = fixed.Int26_6(<-ch)
-//	}
+	//go axiprotocol.ReadDisable(memReadAddr, memReadData)
 
 
+	//Read in the first input batch
 	for i := 0; i < INP_LAYER_SIZE ; i++{
 	 
 	 layer_in[i] = fixed.Int26_6(aximemory.ReadUInt32(memReadAddr, memReadData, false, addrIn + uintptr(4*i)))
